2b1d9d4a6e2206feca8ea2bb128cf876fae0ae32
Carl MH Barenbrug <60690081+carlbarenbrug@users.noreply.github.com>
2021-12-26 11:06:09 +0000

    Dark mode test11

--- owl/site.js
+++ owl/site.js
@@ -6,11 +6,12 @@ console.log("document.getElementById", document.getElementById)
 const darkButton = document.getElementsByClassName("switch");
 console.log("document.getElementById('dark')", document.getElementById('dark'))
 console.log("document.getElementByClassName)", document.getElementsByClassName("switch"))
-// const body = document.body;
-// console.log(document.body)
+
 const body = console.log(document.getElementsByTagName("body"))
 darkButton.onclick = () => {
+  console.log('adding tag to body: ', body)
   body.classList.add('dark');
+  console.log(body)
 };
 
 
