e326c75a4c9a8c7c6a753c74cc47b7aad4ffdc67
Carl MH Barenbrug <60690081+carlbarenbrug@users.noreply.github.com>
2021-12-27 19:34:11 +0000

    Dark mode test21

--- owl/site.js
+++ owl/site.js
@@ -1,11 +1,11 @@
 document.addEventListener
   ("keydown", function(event) {
     if (event.key === "1") {
-      document.documentElement.setAttribute('data-theme', 'dark');
-      localStorage.setItem('theme', 'dark');
-    }
-    if (event.key === "2") {
       document.documentElement.setAttribute('data-theme', 'light');
       localStorage.setItem('theme', 'light');
+    }
+    if (event.key === "2") {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
     }
 });
